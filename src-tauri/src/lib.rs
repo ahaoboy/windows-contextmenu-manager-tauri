be8a374b648ac8cf788283c007459d4b2f0645c2
ahaoboy <504595380@qq.com>
2025-07-17 14:01:31 +0800

    add single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use tauri::Manager as _;
 use wcm::{Manager, Scope, Type};
 
 #[tauri::command]
@@ -81,7 +82,12 @@ fn backup(ty: Type, scope: Option<Scope>) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             list,
