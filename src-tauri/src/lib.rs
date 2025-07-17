3cbef64d0cd0bdbf9e458dc1c2b2eb624fd0d995
ahaoboy <504595380@qq.com>
2025-07-17 13:59:20 +0800

    add single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -81,6 +81,7 @@ fn backup(ty: Type, scope: Option<Scope>) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             list,
