9fb24659b5a2decfa19167157beb339d1d4ba5b4
ahaoboy <504595380@qq.com>
2025-07-19 22:26:25 +0800

    dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@ant-design/v5-patch-for-react-19";
-import { ConfigProvider, theme } from "antd";
 import "./i18n";
-const { defaultAlgorithm, darkAlgorithm } = theme;
-const isDark = globalThis.matchMedia("(prefers-color-scheme: dark)").matches;
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{ algorithm: isDark ? darkAlgorithm : defaultAlgorithm }}
-    >
-      <App />
-    </ConfigProvider>
+    <App />
   </StrictMode>,
 );
