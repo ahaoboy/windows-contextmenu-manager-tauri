e6d950094adb1eea86038b3f5cf579a7f8f6c899
ahaoboy <504595380@qq.com>
2025-07-25 13:53:06 +0800

    add admin

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,32 +1,20 @@
 fn main() {
+    // https://docs.rs/tauri-build/latest/tauri_build/struct.WindowsAttributes.html#example
     let mut windows = tauri_build::WindowsAttributes::new();
     windows = windows.app_manifest(
         r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-  <dependency>
-    <dependentAssembly>
-      <assemblyIdentity
-        type="win32"
-        name="Microsoft.Windows.Common-Controls"
-        version="6.0.0.0"
-        processorArchitecture="*"
-        publicKeyToken="6595b64144ccf1df"
-        language="*"
-      />
-    </dependentAssembly>
-  </dependency>
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-        <requestedPrivileges>
-            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-        </requestedPrivileges>
-    </security>
+      <security>
+          <requestedPrivileges>
+              <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+          </requestedPrivileges>
+      </security>
   </trustInfo>
 </assembly>
-"#
-        .trim(),
+"#,
     );
-    tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
-        .expect("failed to run build script");
+    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
+    tauri_build::try_build(attrs).expect("failed to run build script");
     // tauri_build::build()
 }
